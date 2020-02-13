51f71e61d249c8ecd3c33a8ebca62550d2b265cd
Kamekure-Maisuke <taisukeoono350@gmail.com>
2020-02-13 21:12:19 +0900

    add-todolist

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,16 @@
+<script>
+	let todos = [];
+	const add = () => {todos = [...todos,'']}
+	const remove = (index) => {todos = [...todos.slice(0,index), ...todos.slice(index+1)]}
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>info about</h1>
-
-<p>write about</p>
+<h1>todo list</h1>
+{#each todos as todo, index}
+	<input bind:value={todos[index]}>
+	<button on:click={()=>remove(index)}>削除</button><br>
+{/each}
+<button on:click={add}>追加</button>
